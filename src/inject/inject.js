45e560b452df18259344534f5ca2e4c40c52f3b2
Discountrobot <Discountrobot@users.noreply.github.com>
2015-07-25 14:56:14 +0200

    Update inject.js
    added some comments

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,7 +2,7 @@
 var GIF      = "image/gif",
     GIFV_EXT = ".gifv";
 
-// sigh ...
+// check if the content is "image/gif" regardless of the url extension
 if(document.contentType === GIF) {
       
   // stop loading that shit        
@@ -10,6 +10,7 @@ if(document.contentType === GIF) {
   
   var path   = location.pathname,
       origin = location.origin,
+      // helper method for replacing the url extension
       replaceExtension = function (ext) {
           return origin + path.substr(0, path.lastIndexOf(".")) + ext;
       };
